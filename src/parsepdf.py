3b24d581bb5593e25b9e0be35a2e23512e5bfb62
Alex Pavloff <alex@pavloff.net>
2025-01-25 14:58:26 -0700

    pdf turning into dataframe

--- src/parsepdf.py
+++ src/parsepdf.py
@@ -1,8 +1,47 @@
 import tabula
 
-pdf_path = "C:\\fundev\\longmont_planning_bot\\data\\Active_Developments_Week_1_Jan_2025.pdf"
+import os
+import pandas as pd
 
-dfs = tabula.read_pdf(pdf_path, lattice=True, pages='all')
-# read_pdf returns list of DataFrames
-print(len(dfs))
-print(dfs[0])
+PLANNING_STAGES = [ 'DEVELOPMENT REVIEW', 'PENDING RESUBMITTAL', 'PUBLIC HEARING', 'APPROVED', 'UNDER CONSTRUCTION', 'WITHDRAWN', 'CLOSED' ]
+
+def spreadsheet_to_dataframe(filename):
+    # read spreadsheet into the per-page frame
+    dfs = tabula.read_pdf( filename, lattice=True, pages='all', pandas_options={'header': None})
+
+    # combine them first into a single dataframe
+    combined = pd.concat( dfs, ignore_index=True )
+
+    last_stage = None
+    clean_df = None
+    
+    # Brute force find the stage headers 
+    for i in range(0,len(combined)):
+        firstcell = combined.iloc[i,0]
+        for stage in PLANNING_STAGES:
+            if firstcell == stage:
+                if last_stage is not None:
+                    # We've found a complete stage, now turn it into a dataframe
+                    # actual data is after the header...
+                    data = combined.iloc[ last_stage[1] + 2: i ].copy()
+                    # set the header
+                    data.columns = list(combined.iloc[last_stage[1]+1])
+                    # set the stage of all of these
+                    data['Stage'] = last_stage[0]
+                    # add to the combined version
+                    clean_df = pd.concat( [clean_df, data], ignore_index=True)
+                last_stage = (stage, i)
+
+    clean_df = clean_df.fillna('')
+    return clean_df
+
+def list_pdfs(folder_path):
+    pdf_files = [f for f in os.listdir(folder_path) if f.endswith('.pdf')]
+    return pdf_files
+
+folder_path = 'C:\\fundev\\longmont_planning_bot\\data\\'  # Replace this with the path to your folder
+pdf_files = list_pdfs(folder_path)
+
+for pdf in pdf_files:
+    df = spreadsheet_to_dataframe( os.path.join(folder_path, pdf) )
+    print(df)
